79a9884fc91bcc9ba0b7a3452d769a62d7105fd1
Unknown <budiebasri@gmail.com>
2018-05-31 14:31:53 +0800

    Insert neccessary libraby

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Material Design Bootstrap</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="lib/font-awesome-4.7.0/css/font-awesome.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="lib/MDB/css/bootstrap.min.css" >
+    <!-- Material Design Bootstrap -->
+    <link href="lib/MDB/css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <?php
+   include 'login/login-view.php';
+    ?>
+    <h1></h1>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="lib/MDB/js/jquery-3.2.1.min.js"></script>
+    <script src="lib/MDB/js/popper.min.js"></script>
+    <script src="lib/MDB/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="lib/MDB/js/mdb.min.js"></script>
+  </body>
+</html>
